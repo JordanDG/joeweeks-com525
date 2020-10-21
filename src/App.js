ac95b94e68ad346290635e6903087dff572508e6
Jordan Da Graca <55832919+JordanDG@users.noreply.github.com>
2020-10-21 10:13:10 +0100

    Added Support For React Router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Week1 from "./components/week-1/Week1.js";
 import Week2 from "./components/week-2/Week2.js";
-import Week3 from "./components/week-3/Week3.js";
+import Styling from "./style.css";
 
+// import Week3 from "./components/week-3/Week3.js";
+//<Route path="/week-3">
+//  <Week3 />
+//</Route>
+//
+
+const HomeNav = () => (
+  <nav>
+    <h1>Welcome to Interaction Design!</h1>
+    <ul>
+      <li>
+        <Link to="/week-1">Week1</Link>
+      </li>
+      <li>
+        <Link to="/week-2">Week2</Link>
+      </li>
+      <li>
+        <Link to="/week-3">Week3</Link>
+      </li>
+    </ul>
+  </nav>
+)
 
 function App() {
   return (
@@ -15,11 +37,8 @@ function App() {
         <Route path="/week-2">
           <Week2 />
         </Route>
-        <Route path="/week-3">
-          <Week3 />
-        </Route>
         <Route path="/">
-          <Home />
+          <HomeNav />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/week-1/Week1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Week1() {
+    return (
+        <div>
+            <h2>This is week 1</h2>
+        </div>
+    );
+}
+
+export default Week1;
