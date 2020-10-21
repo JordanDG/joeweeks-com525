994cabae2a9d7120463ba4e570101250cec941ea
Jordan Da Graca <55832919+JordanDG@users.noreply.github.com>
2020-10-21 09:13:46 +0100

    Updates

---
+++ precache-manifest.70ec0d2b7c3c6a4199fc8cbe4b6381b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c3ebd40c79d35228b19257f023140e",
+    "url": "/joeweeks-com525/index.html"
+  },
+  {
+    "revision": "58ae4502e103edf1bf33",
+    "url": "/joeweeks-com525/static/js/2.9361870d.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/joeweeks-com525/static/js/2.9361870d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deaf6bd31d55859338f9",
+    "url": "/joeweeks-com525/static/js/main.0415b86c.chunk.js"
+  },
+  {
+    "revision": "d4f0edb15ba4a72b3af3",
+    "url": "/joeweeks-com525/static/js/runtime-main.eb851877.js"
+  }
+]);
